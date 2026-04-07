a63fd6624b4733986b490cb2931fa715a471e2e5
Vishnu Shankar Tripathi <108225421+vishnu-s-tripathi06@users.noreply.github.com>
2026-04-07 18:41:32 +0530

    Add files via upload

---
+++ CompositionExample.py
@@ -0,0 +1,21 @@
+class Engine:
+    def __init__(self,horse_power):
+        self.horse_power=horse_power
+        
+        
+class Wheel:
+    def __init__(self,size):
+        self.size=size
+        
+class Car:
+    def __init__(self,make,model,horse_power,Wheel_size):
+        self.make=make
+        self.model=model
+        self.engine=Engine(horse_power)
+        self.wheel_size=[Wheel(Wheel_size) for wheel in range(4)]
+    def display(self):
+        return [f"Make:{self.make}   Model:{self.model}   Hourse Power:{self.engine.horse_power}   Wheel Size: {self.wheel_size[0].size} "]
+
+#def __init__(self,make,model,horse_power,Wheel_size):
+car=Car("ford","mustang",900,18)
+print(car.display())
